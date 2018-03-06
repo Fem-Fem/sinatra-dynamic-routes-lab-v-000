6b6363da9cdb9635b02cbe0e1f256cdc0f027b1b
Obafemi Fadairo <ofpupp@princeton.edu>
2018-03-06 14:08:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -33,15 +33,15 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     operation = params[:operation]
     return operation
-    # if operation == "*"
-    #   answer = params[:number].to_i * params[:number].to_i
-    # elsif operation == "+"
-    #   answer = params[:number].to_i + params[:number].to_i
-    # elsif operation == "-"
-    #   answer = params[:number].to_i - params[:number].to_i
-    # else
-    #   answer = params[:number].to_i / params[:number].to_i
-    # end
+    if operation == "*"
+      answer = params[:number].to_i * params[:number].to_i
+    elsif operation == "+"
+      answer = params[:number].to_i + params[:number].to_i
+    elsif operation == "-"
+      answer = params[:number].to_i - params[:number].to_i
+    else
+      answer = params[:number].to_i / params[:number].to_i
+    end
     # return answer.to_s
   end
 end
