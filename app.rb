ceaace26b79b2e9e9c9cf03b28707c7c83ebe375
Obafemi Fadairo <ofpupp@princeton.edu>
2018-03-06 13:57:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
   
   get '/square/:number' do
-    return "#{params[:number]}.to_i * #{params[:number].to_i}"
+    return "#{params[:number].to_i} * #{params[:number].to_i}"
   end
   
   get '/say/:number/:phrase' do
